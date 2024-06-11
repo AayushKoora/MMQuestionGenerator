18968483733064ca918d3d6cd89cdbd46b65d192
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2024-06-11 18:12:14 -0500

    Converted app interface to use methods for better logic in seperating code.

--- src/com/mathminds/AppInterface.java
+++ src/com/mathminds/AppInterface.java
@@ -6,50 +6,70 @@ import java.awt.*;
 public class AppInterface {
 
 
+    int width;
+    int height;
 
-    public static void main(String[] args) {
-        System.out.println("booting first screen.");
+    JFrame frame;
 
-        int width = Toolkit.getDefaultToolkit().getScreenSize().width;
-        int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+    JPanel headerPanel;
+    JPanel questionPanel;
+    JPanel testPanel;
 
-        JFrame frame = new JFrame("TopLevelDemo");
+    JScrollPane questionScrollPane;
+    JScrollPane templateScrollPane;
+
+
+
+    public AppInterface() {
+        width = Toolkit.getDefaultToolkit().getScreenSize().width;
+        height = Toolkit.getDefaultToolkit().getScreenSize().height;
+
+        frame = new JFrame("TopLevelDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
 
-        //header
-        JPanel headerPanel = new JPanel(new BorderLayout());
+    private void createHeaderPanel() {
+        headerPanel = new JPanel(new BorderLayout());
         headerPanel.setPreferredSize(new Dimension(width, (int) (height * 0.1)));
         headerPanel.setBackground(Color.red);
         frame.getContentPane().add(headerPanel, BorderLayout.NORTH);
+    }
 
-
-        //question panel
-        JPanel questionPanel = new JPanel(new BorderLayout());
+    private void createQuestionPanel() {
+        questionPanel = new JPanel(new BorderLayout());
         questionPanel.setPreferredSize(new Dimension(width, (int) (height * 0.55)));
         questionPanel.setBackground(Color.GREEN);
 
-        JScrollPane questionScrollPane = new JScrollPane(questionPanel);
+        questionScrollPane = new JScrollPane(questionPanel);
         questionScrollPane.setPreferredSize(new Dimension((int) (width * 0.35), questionPanel.getHeight()));
         questionScrollPane.setBackground(Color.CYAN);
 
+        templateScrollPane = new JScrollPane(questionPanel);
+        templateScrollPane.setPreferredSize(new Dimension((int) (width * 0.35), questionPanel.getHeight()));
+        templateScrollPane.setBackground(Color.MAGENTA);
+
         frame.getContentPane().add(questionPanel, BorderLayout.CENTER);
+    }
 
-
-        //test panel
-        JPanel testPanel = new JPanel(new BorderLayout());
+    private void createTestPanel() {
+        testPanel = new JPanel(new BorderLayout());
         testPanel.setPreferredSize(new Dimension(width, (int) (height * 0.35)));
         testPanel.setBackground(Color.BLUE);
         frame.getContentPane().add(testPanel, BorderLayout.SOUTH);
+    }
 
 
 
+    public static void main(String[] args) {
+        System.out.println("booting first screen.");
+        AppInterface screen = new AppInterface();
+        screen.createHeaderPanel();
+        screen.createQuestionPanel();
+        screen.createTestPanel();
 
-
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-
-
+        //display window
+        screen.frame.pack();
+        screen.frame.setVisible(true);
     }
 }
