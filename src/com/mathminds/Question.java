0bb1cdf0727cbbc027859dac9dfc68454458853d
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2024-06-11 10:54:11 -0500

    Created system to randomly generate values for fields.

--- src/com/mathminds/Question.java
+++ src/com/mathminds/Question.java
@@ -3,27 +3,55 @@ package com.mathminds;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class Question {
 
     public int templateId = -1;
     public String type;
     public String templateText;
-    public ArrayList<Double> fields;
+    public ArrayList<String> fieldNames;
+    public HashMap<String, Double> fields;
     public Method solver;
 
 
-    public Question(int id, String type, String templateText, ArrayList<Double> fields, Method solver) {
+    public Question(int id, String type, String templateText, ArrayList<String> fieldNames, HashMap<String, Double> fields, Method solver) {
         this.templateId = id;
         this.type = type;
         this.templateText = templateText;
+        this.fieldNames = fieldNames;
         this.fields = fields;
         this.solver = solver;
     }
 
 
+    public void randomizeFields() {
+        for (Map.Entry<String, Double> entry : fields.entrySet()) {
+
+            String type = entry.getKey().split("_")[1].split(":")[0];
+            double lowerBound = Double.parseDouble(entry.getKey().split(":")[1].split("-")[0]);
+            double upperBound = Double.parseDouble(entry.getKey().split("-")[1].split("_")[0]);
+
+            Random random = new Random();
+            double newVal = switch (type) {
+                case "int" -> random.nextInt((int) lowerBound, (int) upperBound);
+                case "double" -> random.nextDouble(lowerBound, upperBound);
+                default -> -1.0;
+            };
+
+            fields.put(entry.getKey(), newVal);
+        }
+    }
+
+
     public String withFieldsInserted() {
         String output = templateText;
+
+        for (String field : fieldNames) {
+            output = output.replace(field, ""  + fields.get(field));
+        }
 
         return output;
     }
