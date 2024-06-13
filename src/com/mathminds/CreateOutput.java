ea4342dd53046a2cc38c9acd0840a9c4c3567c3b
Joseph Kopecky <josephkopecky@jmkopecky.tech>
2024-06-13 17:06:01 -0500

    Figured out how to create a directory and save a file to the user's device.

--- src/com/mathminds/CreateOutput.java
+++ src/com/mathminds/CreateOutput.java
@@ -1,6 +1,14 @@
 package com.mathminds;
 
 
+import com.lowagie.text.Document;
+import com.lowagie.text.html.simpleparser.HTMLWorker;
+import com.lowagie.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.StringReader;
+
 public class CreateOutput {
 
     public TestContainer test;
@@ -13,6 +21,27 @@ public class CreateOutput {
     public void pdf() {
         //code to generate a pdf file and save it to the user's device
         System.out.println("Attempting to generate a pdf with object: " + test);
+
+        try {
+            File newDir = new File(System.getProperty("user.home") + "/Documents/MathMindsOutput");
+            newDir.mkdir();
+
+
+            Document document = new Document();
+            String fileDestination = System.getProperty("user.home") + "/Documents/MathMindsOutput/";
+            fileDestination += test.title += ".pdf";
+            PdfWriter.getInstance(document, new FileOutputStream(fileDestination));
+
+            document.open();
+            String htmlString = "<html><body> This is my Project </body></html>";
+            HTMLWorker htmlWorker = new HTMLWorker(document);
+            htmlWorker.parse(new StringReader(htmlString));
+            document.close();
+
+            System.out.println("PDF created successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
